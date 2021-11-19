ef14a8c5fe5f9887d6cc70709347ef57139c1c43
Aleksandr.Goltsev <Aleksandr.Goltsev@indusoft.ru>
2021-11-19 16:12:44 +0700

    Добавьте файлы проекта.

---
+++ Program/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using Vector;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var tempTask = new Vector(); if (tempTask.taskData[0].Task != null) ;
+                try
+            {
+                var vector1 = new Vector(3, 6, 7);
+                var vector2 = new Vector(4, 8, 1);
+
+                Console.WriteLine("Операции над векторами:");
+                vector1.VectorInfo(vector1);
+                vector2.VectorInfo(vector2);
+
+                Console.WriteLine("\nДлинна вектора 1: " + vector1.Length(vector1));
+                Console.WriteLine("Длинна вектора 2: " + vector1.Length(vector2));
+
+                Console.WriteLine("\nСкалярное произведение векторов:" + vector1.ScalarMultiple(vector1, vector2));
+
+                Console.WriteLine("\nПроизведение двух векторов: ");
+                vector1.VectorInfo(vector1.VectorMultiple(vector1, vector2));
+
+                Console.WriteLine("\nСумма векторов:");
+                vector1.VectorInfo(vector1.VectorSum(vector1, vector2));
+
+                Console.WriteLine("\nРазность векторов:");
+                vector1.VectorInfo(vector1.VectorDifference(vector1, vector2));
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Возникло исключение ", ex);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Program/Vector.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    class Vector
+    {
+        public double x;
+        public double y;
+        public double z;
+
+        public Vector(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+        public void VectorInfo(Vector a)
+        {
+            Console.WriteLine("x: " + a.x + ", y: " + a.y + ", z: " + a.z);
+        }
+        public double Length(Vector a)
+        {
+            return Math.Sqrt((a.x * a.x) + (a.y * a.y) + (a.z * a.z));
+        }
+        public double ScalarMultiple(Vector a, Vector b)
+        {
+            return (a.x * b.x) + (a.y * b.y) + (a.z * b.z);
+        }
+        public Vector VectorMultiple(Vector a, Vector b)
+        {
+            Vector vector = new Vector((a.y * b.z - a.z * b.y), (a.z * b.x - a.x * b.z), (a.x * b.y - a.y * b.x));
+            return vector;
+        }
+        public Vector VectorDifference(Vector a, Vector b)
+        {
+            Vector vector = new Vector((a.x - b.x), (a.y - b.y), (a.z - b.z));
+            return vector;
+        }
+        public Vector VectorSum(Vector a, Vector b)
+        {
+            Vector vector = new Vector((a.x + b.x), (a.y + b.y), (a.z + b.z));
+            return vector;
+        }
+
+    }
+}
